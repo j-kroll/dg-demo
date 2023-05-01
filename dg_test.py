9cc99b062e2f8adba4627aac3c0e3cc6e38fb2b5
j-kroll <jkroll704@gmail.com>
2023-04-30 22:17:19 -0500

    Use smart format, find keyword in full transcript and sentence

--- dg_test.py
+++ dg_test.py
@@ -4,7 +4,7 @@ from secrets import DEEPGRAM_API_KEY
 
 # Based on https://developers.deepgram.com/documentation/getting-started/prerecorded/
 
-FILE = 'life-moves-pretty-fast.wav'
+FILE = '/users/julia/Downloads/PM_why_zoos_cant_buy_sell_animals.mp3'
 MIMETYPE = 'audio/wav'
 
 async def main():
@@ -27,16 +27,36 @@ async def main():
     deepgram.transcription.prerecorded(
       source,
       {
-        'punctuate': True,
+        'smart_format': True,
+        'utterances': True,
         'model': 'nova',
       }
     )
   )
 
-  print(json.dumps(response, indent=4))
+  # print(json.dumps(response, indent=4))
 
   # Write only the transcript to the console
   #print(response["results"]["channels"][0]["alternatives"][0]["transcript"])
+
+  keyword = "lemurs"
+
+  full_transcript = response["results"]["channels"][0]["alternatives"][0]["transcript"]
+
+  if keyword in full_transcript: # TODO stem/lemmatize for not exact matches, "lemur" vs "lemurs"
+    print(f"Keyword {keyword} found in transcript!")
+
+  sliced_words = [w["word"] for w in response["results"]["channels"][0]["alternatives"][0]["words"]]
+
+  if keyword in sliced_words:
+    print(f"Keyword {keyword} found in words!")
+
+  paragraphs = response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"]
+  for p in paragraphs:
+    sentences = [s["text"] for s in p["sentences"]]
+    for s in sentences:
+      if keyword in s:
+        print(f"Found keyword {keyword} in sentence: {s}")
 
 try:
   asyncio.run(main())
