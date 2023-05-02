6f43d9f83d104f7f70756a0bdbe85752a7e46a9e
j-kroll <jkroll704@gmail.com>
2023-05-01 21:52:31 -0500

    Stub readme, clean up print statements

--- README.md
+++ README.md
@@ -1,9 +1,41 @@
 # dg-demo
 
+## About
+
+Search for keywords in a corpus of audio files. Inspect both the text and audio of the sentences containing the keyword.
+
 ## Setup
 
-Install the requirements:
+1. Install the requirements:
 ```
 pip3 install -r requirements.txt
 ```
 
+1. Add your API key in `secrets.py`
+
+## Run the code:
+
+```
+python3 dg_demo.py
+```
+
+## Example output
+
+Given a keyword "lemurs" and the podcast [Why zoos can't buy or sell animals](https://play.podtrac.com/npr-510289/edge1.pod.npr.org/anon.npr-mp3/npr/pmoney/2023/04/20230426_pmoney_e58d4524-d165-42f8-9fbd-10472ce664a0.mp3?d=1003&size=16050095&e=1172161875&t=podcast&p=510289&awEpisodeId=1172161875&awCollectionId=510289&sc=siteplayer&aw_0_1st.playerid=siteplayer):
+
+```
+% python3 dg_demo.py
+Finding keyword: 'lemurs'
+---> Still on the list, lemurs.
+Playing segment from 967105.04 to 969125.0 milliseconds.
+Input #0, wav, from '/var/folders/1j/t0lwqrz93gj0np_r1sdf505w0000gn/T/tmp2juu3a0g.wav':
+  Duration: 00:00:02.02, bitrate: 1411 kb/s
+  Stream #0:0: Audio: pcm_s16le ([1][0][0][0] / 0x0001), 44100 Hz, 2 channels, s16, 1411 kb/s
+   1.94 M-A:  0.000 fd=   0 aq=    0KB vq=    0KB sq=    0B f=0/0   
+---> Calgary wants black and white lemurs.
+Playing segment from 969905.0 to 971765.0 milliseconds.
+Input #0, wav, from '/var/folders/1j/t0lwqrz93gj0np_r1sdf505w0000gn/T/tmpgxxz3t0q.wav':
+  Duration: 00:00:01.86, bitrate: 1411 kb/s
+  Stream #0:0: Audio: pcm_s16le ([1][0][0][0] / 0x0001), 44100 Hz, 2 channels, s16, 1411 kb/s
+   1.79 M-A:  0.000 fd=   0 aq=    0KB vq=    0KB sq=    0B f=0/0     
+```
--- dg_demo.py
+++ dg_demo.py
@@ -10,7 +10,7 @@ from pydub.playback import play
 from secrets import DEEPGRAM_API_KEY
 
 
-file_path = "https://play.podtrac.com/npr-510325/edge1.pod.npr.org/anon.npr-mp3/npr/indicator/2023/04/20230427_indicator_d90de0fd-411e-4022-9987-d8653ea58561.mp3"
+file_path = "https://play.podtrac.com/npr-510289/edge1.pod.npr.org/anon.npr-mp3/npr/pmoney/2023/04/20230426_pmoney_e58d4524-d165-42f8-9fbd-10472ce664a0.mp3"
 file_type = file_path.split(".")[-1]
 mimetype = f"audio/{file_type}"
 audio_segment = AudioSegment.from_file(BytesIO(requests.get(file_path).content), format=file_type)
@@ -44,17 +44,10 @@ async def main():
   # f = open("zoo_transcript.json")
   # response = json.loads(f.read())
 
-  keyword = "tiktok".lower() # "lemurs".lower()
+  keyword = "lemurs".lower()
+  # TODO stem/lemmatize for not exact matches, "lemur" vs "lemurs"
 
-  full_transcript = response["results"]["channels"][0]["alternatives"][0]["transcript"]
-
-  if keyword in full_transcript.lower(): # TODO stem/lemmatize for not exact matches, "lemur" vs "lemurs"
-    print(f"Keyword {keyword} found in transcript!")
-
-  sliced_words = [w["word"].lower() for w in response["results"]["channels"][0]["alternatives"][0]["words"]]
-
-  if keyword in sliced_words:
-    print(f"Keyword {keyword} found in words!")
+  print(f"Finding keyword: '{keyword}'")
 
   paragraphs = response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"]
   for p in paragraphs:
@@ -62,7 +55,7 @@ async def main():
     for s in sentences:
       text = s["text"]
       if keyword in text.lower():
-        print(f"Found keyword {keyword} in sentence: {text}")
+        print(f"---> {text}")
         start_ms = s["start"] * 1000
         end_ms = s["end"] * 1000
         print(f"Playing segment from {start_ms} to {end_ms} milliseconds.")
