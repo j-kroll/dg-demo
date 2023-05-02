fe368846a8ec3b5f8ed75ce8ddb2ca3db8a1df2f
j-kroll <jkroll704@gmail.com>
2023-05-01 21:28:06 -0500

    Get audio slice, get audio from url

--- dg_test.py
+++ dg_test.py
@@ -1,11 +1,19 @@
+import asyncio
+from io import BytesIO
+import json
+import requests
+import sys
+
 from deepgram import Deepgram
-import asyncio, json
+from pydub import AudioSegment
+from pydub.playback import play
+
 from secrets import DEEPGRAM_API_KEY
 
-# Based on https://developers.deepgram.com/documentation/getting-started/prerecorded/
 
-FILE = '/users/julia/Downloads/PM_why_zoos_cant_buy_sell_animals.mp3'
-MIMETYPE = 'audio/wav'
+FILE = 'https://play.podtrac.com/npr-510325/edge1.pod.npr.org/anon.npr-mp3/npr/indicator/2023/04/20230427_indicator_d90de0fd-411e-4022-9987-d8653ea58561.mp3'
+MIMETYPE = 'audio/mp3' #'audio/wav'
+audio_segment = AudioSegment.from_file(BytesIO(requests.get(FILE).content), format=FILE.split(".")[-1])
 
 async def main():
 
@@ -17,7 +25,6 @@ async def main():
     }
   else:
     audio = open(FILE, 'rb')
-
     source = {
       'buffer': audio,
       'mimetype': MIMETYPE
@@ -34,33 +41,30 @@ async def main():
     )
   )
 
-  # print(json.dumps(response, indent=4))
+  # f = open("zoo_transcript.json")
+  # response = json.loads(f.read())
 
-  # Write only the transcript to the console
-  #print(response["results"]["channels"][0]["alternatives"][0]["transcript"])
-
-  keyword = "lemurs"
+  keyword = "tiktok".lower() # "lemurs".lower()
 
   full_transcript = response["results"]["channels"][0]["alternatives"][0]["transcript"]
 
-  if keyword in full_transcript: # TODO stem/lemmatize for not exact matches, "lemur" vs "lemurs"
+  if keyword in full_transcript.lower(): # TODO stem/lemmatize for not exact matches, "lemur" vs "lemurs"
     print(f"Keyword {keyword} found in transcript!")
 
-  sliced_words = [w["word"] for w in response["results"]["channels"][0]["alternatives"][0]["words"]]
+  sliced_words = [w["word"].lower() for w in response["results"]["channels"][0]["alternatives"][0]["words"]]
 
   if keyword in sliced_words:
     print(f"Keyword {keyword} found in words!")
 
   paragraphs = response["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"]
   for p in paragraphs:
-    sentences = [s["text"] for s in p["sentences"]]
+    sentences = [s for s in p["sentences"]]
     for s in sentences:
-      if keyword in s:
-        print(f"Found keyword {keyword} in sentence: {s}")
+      if keyword in s["text"].lower():
+        print(f"Found keyword {keyword} in sentence: {s['text']}")
+        start_ms = s["start"] * 1000
+        end_ms = s["end"] * 1000
+        print(f"Playing segment from {start_ms} to {end_ms} milliseconds.")
+        play(audio_segment[start_ms:end_ms])
 
-try:
-  asyncio.run(main())
-except Exception as e:
-  exception_type, exception_object, exception_traceback = sys.exc_info()
-  line_number = exception_traceback.tb_lineno
-  print(f'line {line_number}: {exception_type} - {e}')
+asyncio.run(main())
