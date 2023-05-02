7ec27af67585918e355e9e0763cd0935bc409402
j-kroll <jkroll704@gmail.com>
2023-05-01 21:35:06 -0500

    Stylistic cleanup

--- dg_test.py
+++ dg_test.py
@@ -2,7 +2,6 @@ import asyncio
 from io import BytesIO
 import json
 import requests
-import sys
 
 from deepgram import Deepgram
 from pydub import AudioSegment
@@ -11,32 +10,33 @@ from pydub.playback import play
 from secrets import DEEPGRAM_API_KEY
 
 
-FILE = 'https://play.podtrac.com/npr-510325/edge1.pod.npr.org/anon.npr-mp3/npr/indicator/2023/04/20230427_indicator_d90de0fd-411e-4022-9987-d8653ea58561.mp3'
-MIMETYPE = 'audio/mp3' #'audio/wav'
-audio_segment = AudioSegment.from_file(BytesIO(requests.get(FILE).content), format=FILE.split(".")[-1])
+file_path = "https://play.podtrac.com/npr-510325/edge1.pod.npr.org/anon.npr-mp3/npr/indicator/2023/04/20230427_indicator_d90de0fd-411e-4022-9987-d8653ea58561.mp3"
+file_type = file_path.split(".")[-1]
+mimetype = f"audio/{file_type}"
+audio_segment = AudioSegment.from_file(BytesIO(requests.get(file_path).content), format=file_type)
 
 async def main():
 
   deepgram = Deepgram(DEEPGRAM_API_KEY)
 
-  if FILE.startswith('http'):
+  if file_path.startswith("http"):
     source = {
-      'url': FILE
+      "url": file_path
     }
   else:
-    audio = open(FILE, 'rb')
+    audio = open(file_path, "rb")
     source = {
-      'buffer': audio,
-      'mimetype': MIMETYPE
+      "buffer": audio,
+      "mimetype": mimetype
     }
 
   response = await asyncio.create_task(
     deepgram.transcription.prerecorded(
       source,
       {
-        'smart_format': True,
-        'utterances': True,
-        'model': 'nova',
+        "smart_format": True,
+        "utterances": True,
+        "model": "nova",
       }
     )
   )
@@ -60,8 +60,9 @@ async def main():
   for p in paragraphs:
     sentences = [s for s in p["sentences"]]
     for s in sentences:
-      if keyword in s["text"].lower():
-        print(f"Found keyword {keyword} in sentence: {s['text']}")
+      text = s["text"]
+      if keyword in text.lower():
+        print(f"Found keyword {keyword} in sentence: {text}")
         start_ms = s["start"] * 1000
         end_ms = s["end"] * 1000
         print(f"Playing segment from {start_ms} to {end_ms} milliseconds.")
