4745a371faf8a3621a1f0235eee551b0367a11d9
j-kroll <jkroll704@gmail.com>
2023-05-02 21:49:44 -0500

    Underline keyword match in text sentence

--- dg_demo.py
+++ dg_demo.py
@@ -16,6 +16,11 @@ from sources import SOURCES
 
 
 deepgram = Deepgram(DEEPGRAM_API_KEY)
+ps = PorterStemmer()
+
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+END = '\033[0m'
 
 def get_source(url, file_type):
   mimetype = f"audio/{file_type}"
@@ -53,8 +58,28 @@ async def transcribe(url, audio):
     print(f"Saved transcription: {file_path}")
   return response
 
+def get_keywords_in_transcript(transcript, audio_segment):
+  paragraphs = transcript["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"]
+  for p in paragraphs:
+    sentences = [s for s in p["sentences"]]
+    for s in sentences:
+      text = s["text"]
+      stemmed_words = {ps.stem(w).lower(): w for w in word_tokenize(text)}
+      stemmed_keywords = {ps.stem(k.lower()): k for k in KEYWORDS}
+      keyword_matches = [s_kw if s_kw in stemmed_words.keys() else None for s_kw in stemmed_keywords.keys()]
+      if any(keyword_matches):
+        print_sentence(stemmed_words, keyword_matches, text)
+        start_ms = s["start"] * 1000
+        end_ms = s["end"] * 1000
+        play(audio_segment[start_ms:end_ms])
+
+def print_sentence(stemmed_words, keyword_matches, text):
+  for match in keyword_matches:
+    if match:
+      text = text.replace(stemmed_words[match], UNDERLINE + stemmed_words[match] + END)
+  print(f"---> {text}")
+
 def main():
-  ps = PorterStemmer()
   print(f"Finding keywords: {KEYWORDS}")
   for url in SOURCES:
     print("\n")
@@ -62,18 +87,7 @@ def main():
     audio_segment = AudioSegment.from_file(BytesIO(requests.get(url).content), format=file_type)
     source = get_source(url, file_type=file_type)
     transcript = asyncio.run(transcribe(url, source))
-    paragraphs = transcript["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"]
-    for p in paragraphs:
-      sentences = [s for s in p["sentences"]]
-      for s in sentences:
-        text = s["text"]
-        stemmed_words = [ps.stem(w).lower() for w in word_tokenize(text)]
-        stemmed_keywords = [ps.stem(k.lower()) for k in KEYWORDS]
-        if any([stemmed_kw in stemmed_words for stemmed_kw in stemmed_keywords]):
-          print(f"---> {text}")
-          start_ms = s["start"] * 1000
-          end_ms = s["end"] * 1000
-          play(audio_segment[start_ms:end_ms])
+    get_keywords_in_transcript(transcript, audio_segment)
 
 if __name__ == "__main__":
   main()
